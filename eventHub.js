f28d1fcea9a5f71e63d0f73488a85334f0097a43
李琦 <qi.li@huilianyi.com>
2022-11-17 12:13:35 +0800

    实现发布订阅模式

---
+++ eventHub.js
@@ -0,0 +1,57 @@
+//obj = {click: [callBack1, callBack2]}
+
+class LqEventHub {
+    constructor() {
+        this.eventHubObj = {}
+    }
+    //订阅者
+    subscribe(fnName,fn){
+        if(!this.eventHubObj[fnName]) {
+            this.eventHubObj[fnName] = []
+            this.eventHubObj[fnName].push(fn)
+        }else {
+            this.eventHubObj[fnName].push(fn)
+        }
+    }
+    //发布者
+    publish(){
+        const fnName = [].shift.call(arguments)
+        const fns = this.eventHubObj[fnName]
+        if(!fns || !fns?.length) {
+            return console.error("没有订阅")
+        }
+        for(let i = 0; i < fns.length; i++) {
+            fns[i].apply(this, arguments)
+        }
+    }
+    //移除订阅
+    remove(){
+        const fnName = [].shift.call(arguments)
+        const fns = this.eventHubObj[fnName]
+        if(!fns || !fns?.length) return
+        if(!arguments?.length) {
+            this.eventHubObj[fnName] = []
+            delete  this.eventHubObj[fnName]
+        }else {
+            for(let i = 0 ; i< fns.length; i++) {
+                if([].slice.call(arguments).includes(fns[i])) {
+                    fns.splice(i,1)
+                }
+            }
+        }
+    }
+}
+
+const test = new LqEventHub()
+// const f1 = function f() {
+//     console.log('11111')
+// }
+// const f2 = function f() {
+//     console.log('2222')
+// }
+
+test.subscribe('click', f1)
+test.subscribe('click', f2)
+test.publish("click")
+test.remove("click",f2)
+test.publish("click")
